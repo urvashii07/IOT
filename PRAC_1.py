fa5d00e174f4ea682d4576f28122e72186970288
urvashii07 <71247389+urvashii07@users.noreply.github.com>
2020-09-14 12:07:36 +0530

    Create PRAC_1.py

---
+++ PRAC_1.py
@@ -0,0 +1,14 @@
+import telepot
+token = '1118809473:AAFF7huNpamM27CJUPMgKTA1RyEkbWiuHVw'
+TelegramBot = telepot.Bot(token)
+print (TelegramBot.getMe())
+ 
+Urvashi chat I'd:850363090
+
+https://api.telegram.org/bot1118809473:AAFF7huNpamM27CJUPMgKTA1RyEkbWiuHVw/getUpdates
+
+https://api.telegram.org/bot1118809473:AAFF7huNpamM27CJUPMgKTA1RyEkbWiuHVw/getme
+
+Hiral I'd:928689986
+
+https://api.telegram.org/bot1118809473:AAFF7huNpamM27CJUPMgKTA1RyEkbWiuHVw/sendMessage?chat_id=928689986&text=Hello
